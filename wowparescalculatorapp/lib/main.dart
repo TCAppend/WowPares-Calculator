655d0104d2998acb06faf8f33f892fb8a2d95137
TCJay_Append <123647533+TCBJYT@users.noreply.github.com>
2025-02-21 04:31:08 +0800

    am cooked

--- wowparescalculatorapp/lib/main.dart
+++ wowparescalculatorapp/lib/main.dart
@@ -50,18 +50,12 @@ class _InputData extends State<MyHomePage> {
           ),
       ),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+
+
+
+
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
